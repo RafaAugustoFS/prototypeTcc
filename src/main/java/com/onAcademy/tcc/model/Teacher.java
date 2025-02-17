0b692f5b7718105d48b4e9a540c3abfbc03a3035
Ana Beatriz Loureiro B. <152741754+AnaLouBispo@users.noreply.github.com>
2025-02-17 18:25:26 -0300

    Arrumando  as relações intermediarias

--- src/main/java/com/onAcademy/tcc/model/Teacher.java
+++ src/main/java/com/onAcademy/tcc/model/Teacher.java
@@ -2,11 +2,13 @@ package com.onAcademy.tcc.model;
 
 import java.util.Date;
 import java.util.List;
-
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 @Entity
@@ -23,14 +25,28 @@ public class Teacher {
 	private String identifierCode;
 	private String password;
 	
+	
+	
+	
 	@OneToMany(mappedBy = "recipientTeacher")
 	private List<FeedBackByStudent> feedback ;
 	
-	@OneToMany(mappedBy = "teacher")
-	private List<DisciplineTeacher> disciplineTeachers;
+	@ManyToMany
+	@JoinTable(
+	    name = "teacher_discipline", 
+	    joinColumns = {@JoinColumn(name = "teacher_id")},
+	    inverseJoinColumns = {@JoinColumn(name = "discipline_id")}
+	)
+	private List<Discipline> disciplines;
+
 	
-	@OneToMany(mappedBy = "classTeacher")
-	private List<ClassSt>classTeacher;
+	@ManyToMany
+	@JoinTable(
+	    name = "class_teacher",
+	    joinColumns = {@JoinColumn(name = "teacher_id")}, 
+	    inverseJoinColumns = {@JoinColumn(name = "classSt_id")}
+	)
+	private List<ClassSt> classes;
 	
 	@OneToMany(mappedBy = "createdBy")
 	private List<FeedbackByTeacher> feedbackProfessor;
