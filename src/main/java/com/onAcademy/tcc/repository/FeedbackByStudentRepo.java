eb01245dde4515af75a815b6fa433f446fcf476d
Rafael Augusto <148670173+RafaAugustoFS@users.noreply.github.com>
2025-02-24 16:15:58 -0300

    ✨
    Criando end-point para buscar feedback's por professor.

--- src/main/java/com/onAcademy/tcc/repository/FeedbackByStudentRepo.java
+++ src/main/java/com/onAcademy/tcc/repository/FeedbackByStudentRepo.java
@@ -1,9 +1,11 @@
 package com.onAcademy.tcc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.onAcademy.tcc.model.FeedBackByStudent;
 
 public interface FeedbackByStudentRepo extends JpaRepository<FeedBackByStudent, Long> {
-
+	List<FeedBackByStudent> findByRecipientTeacher_Id(Long recipientTeacherId);
 }
