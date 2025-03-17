59cf109e5abd5a083bedfbf8ab7737554bed67f7
Ana Beatriz Loureiro B. <152741754+AnaLouBispo@users.noreply.github.com>
2025-03-17 16:37:14 -0300

    📝🎨
    arrumando a formatação dos códigos e adicionando documentação

--- src/main/java/com/onAcademy/tcc/repository/FeedbackFormRepo.java
+++ src/main/java/com/onAcademy/tcc/repository/FeedbackFormRepo.java
@@ -1,14 +1,39 @@
 package com.onAcademy.tcc.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.onAcademy.tcc.model.FeedbackForm;
 import com.onAcademy.tcc.model.Student;
 import com.onAcademy.tcc.model.Teacher;
 
+/**
+ * Repositório para a entidade {@link FeedbackForm}.
+ * Esta interface estende {@link JpaRepository}, fornecendo métodos CRUD (Create, Read, Update, Delete)
+ * e operações de paginação e ordenação para a entidade {@link FeedbackForm}.
+ * Além disso, inclui métodos personalizados para verificar a existência de formulários de feedback
+ * e buscar formulários por ID do estudante recipiente.
+ *
+ * @see JpaRepository
+ * @see FeedbackForm
+ */
 public interface FeedbackFormRepo extends JpaRepository<FeedbackForm, Long> {
+
+    /**
+     * Verifica se já existe um formulário de feedback criado por um professor específico,
+     * para um estudante específico e em um determinado bimestre.
+     *
+     * @param createdBy        Professor que criou o formulário.
+     * @param recipientStudent Estudante recipiente do formulário.
+     * @param bimestre         Bimestre ao qual o formulário se refere.
+     * @return `true` se o formulário existir, `false` caso contrário.
+     */
     boolean existsByCreatedByAndRecipientStudentAndBimestre(Teacher createdBy, Student recipientStudent, int bimestre);
+
+    /**
+     * Busca todos os formulários de feedback associados a um estudante específico.
+     *
+     * @param id ID do estudante recipiente.
+     * @return Uma lista de formulários de feedback associados ao estudante.
+     */
     List<FeedbackForm> findByRecipientStudentId(Long id);
 }
