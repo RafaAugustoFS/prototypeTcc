f0012e78f6747ea7159e25c18de7ce30f8779f35
Roger Lemos <154293097+RogerDevL@users.noreply.github.com>
2025-03-18 14:24:44 -0300

    🎨
    Documentação das classes Models.

--- src/main/java/com/onAcademy/tcc/model/Discipline.java
+++ src/main/java/com/onAcademy/tcc/model/Discipline.java
@@ -2,7 +2,6 @@ package com.onAcademy.tcc.model;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +11,33 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+/**
+ * Representa a entidade "Disciplina" no banco de dados.
+ * 
+ * - Mapeia as informações sobre as disciplinas oferecidas no sistema. - Cada
+ * disciplina pode estar associada a várias turmas e a vários professores.
+ * 
+ * A classe contém os seguintes campos: - id: O identificador único da
+ * disciplina, gerado automaticamente pelo banco de dados. - nomeDisciplina: O
+ * nome da disciplina. - turmaDisciplinas: A lista de turmas associadas a esta
+ * disciplina. - teachers: A lista de professores que lecionam esta disciplina.
+ * 
+ * Relacionamentos: - A disciplina possui um relacionamento Many-to-Many com a
+ * entidade "ClassSt", representando as turmas que oferecem essa disciplina. - A
+ * disciplina possui um relacionamento Many-to-Many com a entidade "Teacher",
+ * representando os professores que lecionam a disciplina.
+ * 
+ * Essa classe é persistida no banco de dados e usa as anotações JPA para mapear
+ * seus campos e relacionamentos.
+ * 
+ * @see jakarta.persistence.Entity
+ * @see jakarta.persistence.ManyToMany
+ * @see lombok.Data
+ * @see lombok.Getter
+ * @see lombok.Setter
+ * @see lombok.NoArgsConstructor
+ */
 
 @Entity
 @Data
--- src/main/java/com/onAcademy/tcc/model/Event.java
+++ src/main/java/com/onAcademy/tcc/model/Event.java
@@ -1,15 +1,35 @@
 package com.onAcademy.tcc.model;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.time.LocalTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+
+/**
+ * Representa a entidade "Evento" no banco de dados.
+ * 
+ * - Mapeia as informações sobre os eventos registrados no sistema, como título,
+ * data, horário, local e descrição.
+ * 
+ * A classe contém os seguintes campos: - id: O identificador único do evento,
+ * gerado automaticamente pelo banco de dados. - tituloEvento: O título do
+ * evento. - dataEvento: A data em que o evento ocorrerá. - horarioEvento: O
+ * horário em que o evento ocorrerá. - localEvento: O local onde o evento será
+ * realizado. - descricaoEvento: A descrição detalhada sobre o evento.
+ * 
+ * Essa classe é persistida no banco de dados e usa as anotações JPA para mapear
+ * seus campos.
+ * 
+ * @see jakarta.persistence.Entity
+ * @see jakarta.persistence.GeneratedValue
+ * @see jakarta.persistence.GenerationType
+ * @see lombok.Data
+ */
 
 @Entity
 @Data
